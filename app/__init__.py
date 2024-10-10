4f8e431e66431cb6cff14e6a4f8c9900da3d7813
pgnikolov <pgnikolov@gmail.com>
2024-10-10 21:37:09 +0200

    added cars to database, added cars.html, cars.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(cars_bp, url_prefix='/cars')
+    app.register_blueprint(cars_bp, url_prefix='/')
 
     from .models import User, Car, RentalHistory
     with app.app_context():
--- app/cars.py
+++ app/cars.py
@@ -7,6 +7,7 @@ cars_bp = Blueprint('cars', __name__)
 @cars_bp.route('/cars')
 def list_cars():
     cars = Car.query.all()
+    print(f"Number of cars found: {len(cars)}")  # Add this line for debugging
     return render_template('cars.html', cars=cars)
 
 # Other routes and functions related to cars can be defined here
